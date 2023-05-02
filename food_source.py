c45af76d1b6dc7f0f97f9151c515e7b1c0b6a8e5
alexwerben <crystall.werben@gmail.com>
2023-05-02 21:06:38 +0300

    new methods add

--- food_source.py
+++ food_source.py
@@ -11,19 +11,41 @@ class FoodSource:
                  dimensions_=2):
         self.lower_limit = lower_limit_
         self.upper_limit = upper_limit_
-        self.nectar = nectar_
-        self.probability = probability_
+        self._nectar = nectar_
+        self._probability = probability_
         self.dimensions = dimensions_
-        self.trial_count = 0
+        self._trial_count = 0
+        self._id = np.random.randint(1, 10e6)
         self.parameters = []
+
+    def __lt__(self, other):
+        return self.get_nectar() < other.get_nectar()
 
     def initialize(self):
         for i in range(self.dimensions):
             param = np.random.uniform(self.lower_limit, self.upper_limit)
             self.parameters.append(param)
 
-    def set_parameters(self, parameters_: list):
+    def get_id(self):
+        return self._id
+
+    def reset_trial_count(self) -> None:
+        self._trial_count = 0
+
+    def increment_trial_count(self) -> None:
+        self._trial_count += 1
+
+    def get_trial_count(self) -> int:
+        return self._trial_count
+
+    def set_parameters(self, parameters_: list) -> None:
         self.parameters = parameters_
+
+    def get_probability(self):
+        return self._probability
+
+    def get_nectar(self):
+        return self._nectar
 
     def get_neighboring(self, neighbor_food_source):
         """
@@ -37,7 +59,7 @@ class FoodSource:
         params = deepcopy(self.parameters)
 
         new_param = self.parameters[dim] + \
-                    phi * (self.parameters[dim] - neighbor_food_source.parameters[dim])
+            phi * (self.parameters[dim] - neighbor_food_source.parameters[dim])
 
         params[dim] = new_param
 
