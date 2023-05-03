4112325a6a9fb96579e21315b2d5240d2d8404c6
alexwerben <crystall.werben@gmail.com>
2023-05-03 15:24:32 +0300

    traces in animation add

--- vizualization/graph.py
+++ vizualization/graph.py
@@ -24,6 +24,10 @@ def animate(i):
         arr = lines[i].split(",")
         x1, x2 = float(arr[1]), float(arr[2])
         ax.scatter(x1, x2)
+        if i > 0:
+            arr0 = lines[i-1].split(",")
+            x10, x20 = float(arr0[1]), float(arr0[2])
+            plt.plot((x10, x1), (x20, x2))
 
     ax.text(0, 1.05, f"{i}", ha="center", va="center", transform=ax.transAxes, bbox=dict(facecolor="none"))
 
@@ -31,6 +35,6 @@ def animate(i):
 if __name__ == '__main__':
     x, y, z = make_data()
 
-    ani = animation.FuncAnimation(fig, animate, 9, interval=500, blit=False)
+    ani = animation.FuncAnimation(fig, animate, 99, interval=200, blit=False)
 
     plt.show()
